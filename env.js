9c9cb30bdf480d40acd9e106d58fd15864889fa8
Adam Żaczek <adam.zaczek@netguru.pl>
2017-07-02 23:40:32 +0200

    Add Base Node.js Config

---
+++ env.js
@@ -0,0 +1,4 @@
+// we would normally make it gitignored + we would import it with process.env
+// this is only for our use for now
+
+export default 'xoxp-204446259969-206986230181-207720349479-9b509bb2f23813db90749d77a3975a25';
---
+++ server/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console, no-shadow */
+import express from 'express';
+import graphQLHTTP from 'express-graphql';
+import chalk from 'chalk';
+import bodyParser from 'body-parser';
+import config from './config/environment';
+import schema from './appData/schema';
+import slackToken from '../env';
+
+if (config.env === 'development') {
+  const graphql = express();
+  graphql.use('/', graphQLHTTP({
+    graphiql: true,
+    pretty: true,
+    schema,
+    context: {
+      slackToken,
+    },
+  }));
+  graphql.listen(config.graphql.port, () => console.log(chalk.green(`GraphQL is listening on port ${config.graphql.port}`)));
+  graphql.use(bodyParser.json({ type: '*/*' }));
+} else if (config.env === 'production') {
+  const graphQLServer = express();
+  graphQLServer.use('/graphql', graphQLHTTP({ schema }));
+  graphQLServer.use(bodyParser.json({ type: '*/*' }));
+  graphQLServer.listen(config.port, () => console.log(chalk.green(`GraphQL is listening on port ${config.port}`)));
+}
